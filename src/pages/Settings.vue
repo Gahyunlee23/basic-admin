5281955c9f74b80b63b1fe30f1c4927c30229822
gahyun <gahyunlee1119@gmail.com>
2024-09-04 16:03:49 -0400

    basic setting

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,11 +1,32 @@
-<script setup>
-
-</script>
-
 <template>
-
+    <div class="dashboard-layout">
+        <Sidebar />
+        <div class="dashboard-content">
+            <h2>Setting</h2>
+        </div>
+    </div>
 </template>
 
-<style scoped>
+<script>
+import Sidebar from '@/components/Sidebar.vue';
 
+
+export default {
+    components: {
+        Sidebar,
+    },
+    data() {},
+    async mounted() {},
+};
+</script>
+
+<style scoped>
+.dashboard-layout {
+    display: flex;
+}
+
+.dashboard-content {
+    flex: 1;
+    padding: 20px;
+}
 </style>
